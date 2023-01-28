06e9bff66e2bf538a41b81d71eb88f9806ccb623
Tharlly Alves Martins <tharllymartins16@gmail.com>
2023-01-28 10:12:12 -0300

    Created app Task

---
+++ tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+
+class Task(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    name = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete="CASCADE")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
